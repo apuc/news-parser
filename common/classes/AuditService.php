5225b3e0d2cc3feb2c938390570a32491c4b5ad0
Liza Antonets <glambertessa@gmail.com>
2020-02-13 18:28:51 +0300

    site title

---
+++ common/classes/AuditService.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace common\classes;
+
+
+use http\Exception;
+
+class AuditService
+{
+    public static function getTitle($domain)
+    {
+        try {
+            $Punycode = new Punycode();
+            $page_content = file_get_contents('http://' . $Punycode->encode($domain));
+            preg_match_all( "|<title>(.*)</title>|sUSi", $page_content, $titles);
+            if(count($titles[1]))
+                return $titles[1][0];
+            else return '';
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
+    }
+
+}
