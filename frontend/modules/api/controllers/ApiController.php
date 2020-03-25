288de74bb29e670681859ee0d0d768851ff35b09
akosse <glambertessa@gmail.com>
2020-03-25 11:48:26 +0300

    change path in api

--- frontend/modules/api/controllers/ApiController.php
+++ frontend/modules/api/controllers/ApiController.php
@@ -203,15 +203,11 @@ class ApiController extends Controller
     public function actionTemplates()
     {
         $templates = Template::find()->all();
-        $uri = sprintf(
-            "%s://%s",
-            isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
-            $_SERVER['SERVER_NAME']
-        );
 
         $model = array();
         foreach ($templates as $value) {
-            $img = '<img src="'. $uri .'/workspace/modules/themes/themes/' . $value->name . '/preview.jpg" class="img" />';
+
+            $img = '<img src="https://placement-site.craft-group.xyz/workspace/modules/themes/themes/' . $value->name . '/preview.jpg" class="img" />';
             array_push($model, new Theme($value->id, $value->name, $value->description, $img, $value->version, 'не скачано'));
         }
 
